4a6d7aad3b009927b147264605ae428dfb38b9ae
Савва Чиглинцев <savva_00@bk.ru>
2020-03-02 17:53:28 +0000

    Наделал всякого

--- esoft/esoft/Form1.Designer.cs
+++ esoft/esoft/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(25, 89);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 9;
+            this.enterButton.Text = "Войти";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(12, 63);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Login";
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(12, 24);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(128, 124);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox login;
     }
 }
 
--- esoft/esoft/Form1.cs
+++ esoft/esoft/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace esoft
 {
     public partial class Form1 : Form
     {
+        UpravCoef upravCoef = new UpravCoef();
+        SqlConnection sqlConnection = new SqlConnection("Data source=303-1\\NEW_MSSQLSERVER; Initial Catalog=EsoftDBZP; Integrated Security=true;");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            //создание sql запроса
+            SqlCommand sqlCommand = new SqlCommand(
+                $"SELECT" +
+                    $" executor.Логин," +
+                    $" managers.Логин " +
+                $"FROM" +
+                    $" executor, managers " +
+                $"WHERE" +
+                    $" (executor.Логин = '{login.Text}' AND executor.Пароль = '{password.Text}') OR " +
+                    $" (managers.Логин = '{login.Text}' AND managers.Пароль = '{password.Text}')",
+                sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            if (sqlDataReader.HasRows)
+            {
+                upravCoef.Show();//открытие формы
+            }
+            else
+            {
+                MessageBox.Show("Некорректные логин или пароль");
+            }
+            sqlConnection.Close();
         }
     }
 }
